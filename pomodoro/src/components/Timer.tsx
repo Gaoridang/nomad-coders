b481db9b159daeee9d70bb0a774e8acd7b768a4b
gaoridang2 <monooi@naver.com>
2023-06-09 10:41:06 +0900

    Making the count with useState

--- pomodoro/src/components/Timer.tsx
+++ pomodoro/src/components/Timer.tsx
@@ -1,40 +1,55 @@
 import { useCallback, useEffect, useRef, useState } from 'react';
-import { useRecoilState } from 'recoil';
+import { useRecoilValue } from 'recoil';
 import { minutesState, secondsState } from '../atoms/atom';
 
 const Timer = () => {
-  const [minutes, setMinutes] = useRecoilState(minutesState);
-  const [seconds, setSeconds] = useRecoilState(secondsState);
+  const minutes = useRecoilValue(minutesState);
+  const seconds = useRecoilValue(secondsState);
+  const [countMin, setCountMin] = useState(0);
+  const [countSec, setCountSec] = useState(0);
 
   const intervalRef = useRef<number | null>(null);
+
+  useEffect(() => {
+    const count = minutes * 60 + seconds;
+
+    const min = Math.floor(count / 60);
+    const sec = count - min * 60;
+
+    setCountMin(min);
+    setCountSec(sec);
+  }, [minutes, seconds]);
 
   const startTimer = useCallback(() => {
     if (intervalRef.current !== null) return;
 
     intervalRef.current = window.setInterval(() => {
-      setSeconds((prevSec) => {
-        if (prevSec < 0) {
-          return 59;
-        } else {
+      setCountSec((prevSec) => {
+        if (prevSec > 0) {
           return prevSec - 1;
+        } else {
+          setCountMin((prevMin) => {
+            if (prevMin > 0) {
+              return prevMin - 1;
+            } else {
+              if (intervalRef.current !== null) {
+                clearInterval(intervalRef.current);
+                intervalRef.current = null;
+              }
+              return 0;
+            }
+          });
+          return 59;
         }
       });
     }, 1000);
-  }, [setSeconds]);
-
-  useEffect(() => {
-    if (seconds < 0 && minutes > 0) {
-      setMinutes((prevMin) => prevMin - 1);
-      setSeconds(59);
-    } else if (seconds === 0 && minutes === 0 && intervalRef.current !== null) {
-      clearInterval(intervalRef.current);
-      intervalRef.current = null;
-    }
-  }, [seconds, minutes, setMinutes, setSeconds]);
+  }, []);
 
   return (
     <div>
-      {String(minutes).padStart(2, '0')} : {String(seconds).padStart(2, '0')}
+      <p>
+        {String(countMin).padStart(2, '0')} : {String(countSec).padStart(2, '0')}
+      </p>
       <button onClick={startTimer}>start</button>
     </div>
   );
