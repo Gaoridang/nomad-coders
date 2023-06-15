3bee0ea7debbaa95127b600c4fe4a0c88939fb6f
gaori <monooi@naver.com>
2023-06-15 18:53:52 +0900

    타이머 상수 만들기

---
+++ pomodoro/src/components/TimerNew.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { WORK_TIME } from "../constants";
+
+const TimerNew = () => {
+  const [count, setCount] = useState(WORK_TIME);
+
+  return <div>TimerNew</div>;
+};
+
+export default TimerNew;
