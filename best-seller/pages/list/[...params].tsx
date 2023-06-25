65a9f8dbacbcec9380ef188ba6c9d1474cc63896
gaori <monooi@naver.com>
2023-06-25 23:42:59 +0900

    Styling home page

--- best-seller/pages/list/[...params].tsx
+++ best-seller/pages/list/[...params].tsx
@@ -1,10 +1,14 @@
 import axios from "axios";
+import Image from "next/image";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 interface Book {
   author: string;
   book_image: string;
+  book_image_width: number;
+  book_image_height: number;
   book_url: string;
   buy_links: {
     name: string;
@@ -15,34 +19,55 @@ interface Book {
   primary_isbn10: string;
 }
 
-const id = () => {
+const Detail = () => {
   const router = useRouter();
   const [bestSellers, setBestSellers] = useState<Book[]>([]);
+  const [isLoading, setLoading] = useState(false);
   // as string[] 없으면 왜 안 됨? array 맞는데
   const [listName] = (router.query.params as string[]) || [];
+  console.log(listName, "list-name");
   useEffect(() => {
+    setLoading(true);
+
     (async () => {
       const bestSellers = await axios
         .get(`https://books-api.nomadcoders.workers.dev/list?name=${listName}`)
-        .then((res) => res.data.results.books);
+        .then((res) => {
+          setLoading(false);
+          return res.data.results.books;
+        });
 
       setBestSellers(bestSellers);
       console.log(bestSellers, typeof bestSellers, Array.isArray(bestSellers));
     })();
-  }, []);
+  }, [listName]);
 
-  console.log(router);
+  // console.log(router);
 
   return (
     <>
       <h4>{router.query.id}</h4>
       <div>
-        {bestSellers.map((best) => (
-          <p key={best.primary_isbn10}>{best.title}</p>
+        {isLoading && "Loading..."}
+        {bestSellers?.map((best, index) => (
+          <div key={`${best.primary_isbn10}-${index}`}>
+            <Image
+              src={best.book_image}
+              alt={best.title}
+              width={best.book_image_width}
+              height={best.book_image_height}
+              placeholder="empty"
+              priority={index === 0 ? true : false}
+            />
+            <button>
+              <Link href={best.buy_links[0].url}> Buy Now</Link>
+            </button>
+          </div>
         ))}
+        <style jsx>{``}</style>
       </div>
     </>
   );
 };
 
-export default id;
+export default Detail;
